f7bb1c6c1aa2656457dd93d628ec88caead2efb6
Chris Farris <chrisf@primeharbor.com>
2023-12-15 07:54:17 -0500

    Update with version pinning and all lookups

--- examples/pipeline/main.tf
+++ examples/pipeline/main.tf
@@ -34,7 +34,12 @@ locals {
 }
 
 module "organization" {
+
+  # Use the latest
   source = "github.com/primeharbor/org-kickstart"
+  
+  # Pin to a specific release
+  # source = "github.com/primeharbor/org-kickstart?ref=v0.0.1"
 
   tag_set = local.default_tags
 
@@ -49,10 +54,10 @@ module "organization" {
   session_duration          = lookup(var.organization, "session_duration", "PT8H")
   admin_permission_set_name = lookup(var.organization, "admin_permission_set_name", "AdministratorAccess")
   admin_group_name          = lookup(var.organization, "admin_group_name", "AllAdmins")
-  disable_sso_management    = false
+  disable_sso_management    = lookup(var.organization, "disable_sso_management", false)
 
   # Audit Role
-  deploy_audit_role = true # override managing audit role
+  deploy_audit_role = lookup(var.organization, "deploy_audit_role", true)
   audit_role_name   = lookup(var.organization, "audit_role_name", "security-audit")
 
   # CloudTrail
