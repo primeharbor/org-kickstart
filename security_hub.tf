8808f39fa2148314dae09113cb62e6d13140670e
Chris Farris <chrisf@primeharbor.com>
2024-04-13 13:10:51 -0400

    Add support for Central Configuration

--- security_hub.tf
+++ security_hub.tf
@@ -14,7 +14,6 @@
 
 resource "aws_securityhub_account" "payer_account" {
   count = local.security_services["disable_securityhub"] ? 0 : 1
-  #   provider                  = aws.payer_account
   enable_default_standards  = false
   control_finding_generator = "SECURITY_CONTROL"
   auto_enable_controls      = false
@@ -35,30 +34,70 @@ resource "aws_organizations_delegated_administrator" "securityhub" {
   account_id        = module.security_account.account_id
   service_principal = "securityhub.amazonaws.com"
   depends_on = [
-    aws_securityhub_account.payer_account,
-    aws_securityhub_account.security_account
+    aws_securityhub_account.payer_account[0],
+    aws_securityhub_account.security_account[0]
   ]
 }
 
 # Once both hubs are created, we can delegate admin to the security account
 resource "aws_securityhub_organization_admin_account" "delegated_admin" {
   count = local.security_services["disable_securityhub"] ? 0 : 1
-  #   provider         = aws.payer_account
   depends_on = [
-    aws_securityhub_account.payer_account,
-    aws_securityhub_account.security_account
+    aws_securityhub_account.payer_account[0],
+    aws_securityhub_account.security_account[0]
   ]
   admin_account_id = module.security_account.account_id
+}
+
+resource "aws_securityhub_finding_aggregator" "regional_aggregator" {
+  depends_on = [
+    aws_securityhub_account.security_account[0],
+    aws_securityhub_organization_admin_account.delegated_admin[0]
+  ]
+  count        = local.security_services["disable_securityhub"] ? 0 : 1
+  provider     = aws.security-account
+  linking_mode = "ALL_REGIONS"
 }
 
 # Once the delegation is complete, we create the organization config.
 resource "aws_securityhub_organization_configuration" "security_account" {
   depends_on = [
-    aws_securityhub_account.security_account,
-    aws_securityhub_organization_admin_account.delegated_admin
+    aws_securityhub_account.security_account[0],
+    aws_securityhub_finding_aggregator.regional_aggregator[0],
+    aws_securityhub_organization_admin_account.delegated_admin[0]
   ]
   count                 = local.security_services["disable_securityhub"] ? 0 : 1
   provider              = aws.security-account
-  auto_enable           = true
+  auto_enable           = false
   auto_enable_standards = "NONE"
+
+  organization_configuration {
+    configuration_type = "CENTRAL"
+  }
+}
+
+resource "aws_securityhub_configuration_policy" "no_enabled_standards" {
+  count       = local.security_services["disable_securityhub"] ? 0 : 1
+  provider    = aws.security-account
+  depends_on  = [aws_securityhub_organization_configuration.security_account[0]]
+  name        = "NoStandards"
+  description = "Enable Security Hub Central Configuration without any Standards"
+
+  configuration_policy {
+    service_enabled = true
+    enabled_standard_arns = [
+      #   "arn:aws:securityhub:us-east-1::standards/aws-foundational-security-best-practices/v/1.0.0",
+      #   "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0",
+    ]
+    security_controls_configuration {
+      disabled_control_identifiers = []
+    }
+  }
+}
+
+resource "aws_securityhub_configuration_policy_association" "root_ou" {
+  count     = local.security_services["disable_securityhub"] ? 0 : 1
+  provider  = aws.security-account
+  target_id = aws_organizations_organization.org.roots[0].id
+  policy_id = aws_securityhub_configuration_policy.no_enabled_standards[0].id
 }
