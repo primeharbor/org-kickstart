f2a5c9c6d2cdebd5fcd1a3e7be743a8a6a339d24
Chris Farris <chrisf@primeharbor.com>
2024-12-15 07:41:39 -0500

    cruft removal

--- modules/security_services/guardduty.tf
+++ modules/security_services/guardduty.tf
@@ -26,14 +26,6 @@ resource "aws_guardduty_detector" "security_detector" {
   enable   = true
 }
 
-# Note: this is not needed because the aws_guardduty_organization_admin_account resource superceeds it.
-# resource "aws_organizations_delegated_administrator" "guardduty" {
-#   provider = aws.payer_account
-#   depends_on = [aws_guardduty_detector.payer_detector]
-#   account_id        = var.security_account_id
-#   service_principal = "guardduty.amazonaws.com"
-# }
-
 # Assign delegated admin to the security account via GuardDuty APIs
 resource "aws_guardduty_organization_admin_account" "guardduty" {
   count    = local.security_services["disable_guardduty"] ? 0 : 1
@@ -54,30 +46,3 @@ resource "aws_guardduty_organization_configuration" "organization" {
   detector_id                      = aws_guardduty_detector.security_detector[0].id
 }
 
-# # This adds all the existing accounts to the delegated admin
-# resource "aws_guardduty_member" "member" {
-#   provider    = aws.security_account
-#   depends_on  = [aws_guardduty_organization_configuration.organization]
-#   account_id  = each.key
-#   detector_id = aws_guardduty_detector.security_detector[0].id
-#   email       = each.value["email"]
-#   invite      = false
-
-#   for_each = {
-#     # Thank you Stack Overflow for this
-#     # https://stackoverflow.com/questions/58594506/how-to-for-each-through-a-listobjects-in-terraform-0-12
-#     # https://stackoverflow.com/questions/75524827/is-there-a-way-to-exclude-items-inside-of-a-for-each
-#     # Security Account cannot be a member if itself and must be excluded from the list.
-
-#     # We cannot use count here either, so we must add the disable_guardduty flag
-#     for index, account in data.aws_organizations_organizational_unit_descendant_accounts.accounts.accounts :
-#     account.id => account if account.id != var.security_account_id && !local.security_services["disable_guardduty"]
-#   }
-
-#   lifecycle {
-#     ignore_changes = [
-#       email, invite
-#     ]
-#   }
-
-# }
