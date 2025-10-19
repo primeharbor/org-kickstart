843cd9272e891a34cb3dec45347e9142914dfd9d
Chris Farris <chrisf@primeharbor.com>
2025-10-19 20:05:07 +0100

    Remove commented out standards

--- security_hub.tf
+++ security_hub.tf
@@ -95,10 +95,7 @@ resource "aws_securityhub_configuration_policy" "no_enabled_standards" {
 
   configuration_policy {
     service_enabled = true
-    enabled_standard_arns = [
-      #   "arn:aws:securityhub:us-east-1::standards/aws-foundational-security-best-practices/v/1.0.0",
-      #   "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0",
-    ]
+    enabled_standard_arns = [ ]
     security_controls_configuration {
       disabled_control_identifiers = []
     }
